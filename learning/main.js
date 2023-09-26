049c6016b842746e53f773fd893c70b39484edfc
Daniela-HRF <dhrf2105@gmail.com>
2023-09-26 17:38:43 +0200

    exercises

---
+++ learning/main.js
@@ -0,0 +1,13 @@
+alert("Bonjour, tu veux quitter ce site")
+
+console.log("holaaaa ROBERTO")
+
+var cardName = "ROBERTO"
+var age = 12
+var list = 
+"
+const teacherAge = 28
+const studentAge = 17
+
+if (teacherAge < studentAge) {
+    console.log
