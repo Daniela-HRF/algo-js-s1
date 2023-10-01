be2c33e0406866094c32bc11265024f2ca5392c4
Daniela-HRF <dhrf2105@gmail.com>
2023-10-01 23:35:29 +0200

    le tueur

---
+++ Exo-2-le-tueur/main.js
@@ -0,0 +1,16 @@
+class tueur {
+    constructor(name, pv, caracteristics) {
+        this.name = name
+        this.pv = pv
+        this.caracteristics = caracteristics
+    }
+}
+
+    attack.Tueur(survivors)
+
+let Jason = new tueur ("Jason", 100, "tall, nerd, skinny, smart")
+console.log(Jason)
+
+let survivors = ["Camila","Blake","Kenzy", "Justin", "Maddie"]
+console.log(survivors)
+let caracteristics = ["autoritaire", "sociable", "fier", "orgueilleux", "altruiste"]
